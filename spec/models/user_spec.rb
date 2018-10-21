0ded4d413093e6e51c029882ef8e77515eb138d5
manju dhiman <manju.dhiman@cerner.com>
2018-10-22 00:02:47 +0530

    added specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user validation' do
+    subject { described_class.new(email: "abc@gmail.com", password: "123123", remember_me: "0") }
+
+    context 'When email and passsword are present' do
+      it "user is valid" do
+        expect(subject).to be_valid
+      end
+    end
+  end
 end
